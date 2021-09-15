f908e8b72f235842e0ed61891bac3490f6560808
Ruixin Wang <wangrx1999@gmail.com>
2021-09-15 01:59:35 -0700

    update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,6 +1,5 @@
-
 dataset=$1
-repo_dir=$2
+global_repo_dir=$2
 global_output_dir=$3
 
 fail_to_clone=0
@@ -8,24 +7,28 @@ no_req=0
 missing=0
 success=0
 
-cd $repo_dir
+echo script version: $(git rev-parse HEAD)
+
+parent_dir=$(pwd)
+cd $global_repo_dir
 for i in $(cut -d, -f1 $dataset | uniq | sed '1d'); do
     if [[ ! -d "$i" ]]; then
 	fail_to_clone=`expr $fail_to_clone + 1`
 	echo $i,fail_to_clone >> $global_output_dir/stat.csv
 	continue
     fi
-    cd $i
+    cd $global_repo_dir/$i
     if [[ ! -n "$(find -maxdepth 1 -name "*requirement*")" ]]; then
 	no_req=`expr $no_req + 1`
         echo $i,requirements_not_found >> $global_output_dir/stat.csv
-	cd -
+	cd $global_repo_dir
         continue
     fi
-    # bash install.sh Repo $i victims_brittles.csv tests.csv output;
+    cd $parent_dir
+    bash install.sh $global_repo_dir $i victims_brittles.csv tests.csv output;
     echo $i,can_install>> $global_output_dir/stat.csv
-    cd -
+    cd $global_repo_dir
 done
-cd ..
+cd $parent_dir
 echo $fail_to_clone,$no_req
 
