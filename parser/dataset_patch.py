a2fa68ca2a46ce972f41af13387f25100eb131da
ailen-wrx <ailen-wrx@gmail.com>
2021-10-02 09:16:16 +0000

    introducing process amending Gruber dataset

---
+++ parser/dataset_patch.py
@@ -0,0 +1,26 @@
+from parseMethods import *
+
+with open(dataset_amended, 'w') as f:
+    csv.writer(f).writerow(['Project_Name','Project_URL','Project_Hash','Test_filename','Test_classname','Test_funcname','Test_parametrization','Order-dependent','Verdict_Isolated','Verdict_OriginalOrder'])
+
+with open(dataset_dir, 'rt') as f:
+    r = csv.reader(f)
+    for row in r:
+        project = row[0]
+        if project == 'Project_Name':
+            continue
+        Test_filename = row[3]
+        Test_classname = row[4]
+        Test_funcname = row[5]
+        Test_para = row[6]
+
+        if str(Test_filename).split('/')[-1] != (str(Test_classname).split('.')[-1] + '.py'):
+            ClassName = str(Test_classname).split('.')
+            ClassName[-2] = ClassName[-2] + ".py"
+            row[3] = '/'.join(ClassName[:-1])
+            row[4] = ClassName[-1]
+        else: 
+            row[4] = ''
+        with open(dataset_amended, 'a') as f1:
+            csv.writer(f1).writerow(row)
+
