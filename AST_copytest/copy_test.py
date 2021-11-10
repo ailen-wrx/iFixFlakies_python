3cf4646565a28083569de1170ff1588f1df0aa68
root <root@4f285643609d>
2021-11-10 13:54:41 +0000

    Update patcher.

--- AST_copytest/copy_test.py
+++ AST_copytest/copy_test.py
@@ -3,9 +3,11 @@
 
 import ast
 import csv
-import io
+import difflib
+from py import io
 import re
 import os
+import shutil
 import sys
 import time
 import pytest
@@ -14,7 +16,7 @@ from unparse import Unparser
 from io import StringIO
 import hashlib, binascii
 
-CACHE_DIR='./patchercache/'
+CACHE_DIR='../patchercache/'
 
 
 class get_origin_astInfo(ast.NodeVisitor):
@@ -31,14 +33,39 @@ class get_origin_astInfo(ast.NodeVisitor):
 
 def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath, combination_path,result_path):
 
+    cache_in_tests=[]
     patch_list=[]
-    
+
     victim_path=victim_fullpath.split('::')[0]
     victim_testfunc=victim_fullpath.split('::')[-1]
+    if '[' in victim_testfunc and ']' in victim_testfunc:
+        start=victim_testfunc.index('[')
+        end=victim_testfunc.index(']')+1
+        par=victim_testfunc[start:end]
+        print(par)
+        final=victim_testfunc.replace(str(par),"")
+        #print(final)
+        victim_testfunc=final
+    victim_class=None
+    if len(victim_fullpath.split('::'))>2:
+        victim_class=victim_fullpath.split('::')[-2]
 
     cleaner_path=cleaner_fullpath.split('::')[0]
     cleaner_testfunc = cleaner_fullpath.split('::')[-1]
+    if '[' in cleaner_testfunc and ']' in cleaner_testfunc:
+        start=cleaner_testfunc.index('[')
+        end=cleaner_testfunc.index(']')+1
+        par=cleaner_testfunc[start:end]
+        print(par)
+        final=cleaner_testfunc.replace(str(par),"")
+        #print(final)
+        cleaner_testfunc=final
 
+    cleaner_class=None
+    if len(cleaner_fullpath.split('::'))>2:
+        cleaner_class=cleaner_fullpath.split('::')[-2]
+
+    
     with open(victim_path, "r") as victim:
         tree_victim = ast.parse(victim.read())
        
@@ -48,24 +75,35 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
         cleaner_info = get_origin_astInfo(tree_cleaner)
         cleaner_import_num = cleaner_info.get_import_num()
 
+        
     md5 = get_md5(polluter_fullpath, cleaner_fullpath, victim_fullpath)
 
-    pcv_result=run_tests_pcv(md5, polluter_fullpath, cleaner_fullpath, victim_fullpath)
-    pv_result=run_tests_pv(md5,polluter_fullpath,victim_fullpath)
 
-              
+    try:
+        pcv_result=run_tests_pcv(project, md5, polluter_fullpath, cleaner_fullpath, victim_fullpath)
+        pv_result=run_tests_pv(project, md5,polluter_fullpath,victim_fullpath)
+
+    except:
+        with open(result_path,'a+') as f:
+            csv_write = csv.writer(f)
+            result=[project,sha,polluter_fullpath,cleaner_fullpath,victim_fullpath,md5,'PCV_ERROR']
+            csv_write.writerow(result)
+            exit(1)
+            
+    minimal_patch_file=None
     patch_time_1st = None
     patch_time_all = None
     can_copy_work = None
-
+    
     if pv_result.strip() == 'failed' and pcv_result.strip() =='passed':
         # copy unique Import and ImportFrom modules
+        
         for import_obj in [module for module in ast.walk(tree_cleaner) if
                            isinstance(module, ast.Import) or isinstance(module, ast.ImportFrom)]:
             if ast.dump(import_obj) not in [ast.dump(module) for module in ast.walk(tree_victim) if
                                             isinstance(module, ast.Import) or isinstance(module, ast.ImportFrom)]:
                 tree_victim.body.insert(0, import_obj)
-
+     
         # get helper code from cleaner, handle setup, body and teardown 'module, method, class, function'
         # setup_module,setup_class,setup_function,setup_method,test_body,teardown_method,teardown_function,teardown_class,teardown_module
 
@@ -74,12 +112,16 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
                           'teardown_method': None, 'teardown_function': None, 'teardown_class': None,
                           'teardown_module': None}
 
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print([func.name for func in ast.walk(tree_cleaner) if isinstance(func, ast.FunctionDef)])
         for eachfunc in [func for func in ast.walk(tree_cleaner) if isinstance(func, ast.FunctionDef)]:
             if eachfunc.name in name_node_dict:
                 name_node_dict[eachfunc.name] = eachfunc.body
 
         insert_nodes_keys = [key for key in name_node_dict if name_node_dict[key] != None]
-
+        print(insert_nodes_keys)
+#        print(cleaner_testfunc)
+#        print(insert_nodes_keys)
         pre_node_key = insert_nodes_keys[0]
         pre_node = name_node_dict[pre_node_key]
 
@@ -89,10 +131,21 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
         insert_node = pre_node
 
         # get victim test body
-        for victim_obj in [func for func in ast.walk(tree_victim) if isinstance(func, ast.FunctionDef)]:
-            if victim_obj.name == victim_testfunc:
-                victim_node = victim_obj.body
-                break
+
+        if victim_class:
+            for vic_class in [node for node in ast.walk(tree_victim) if isinstance(node,ast.ClassDef)]:
+                if vic_class.name == victim_class:
+                    for victim_obj in [func for func in ast.iter_child_nodes(vic_class) if isinstance(func,ast.FunctionDef)]:
+                        print(victim_obj)             
+                        if victim_obj.name == victim_testfunc:
+                               victim_node = victim_obj.body
+                               break
+
+        else:
+            for victim_obj in [func for func in ast.walk(tree_victim) if isinstance(func, ast.FunctionDef)]:
+                if victim_obj.name == victim_testfunc:
+                     victim_node = victim_obj.body
+                     break
 
     
 
@@ -107,24 +160,28 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
         tmp_insert_node = insert_node
 
         tmp_origin_victim.insert(0, tmp_insert_node)
-        ast.fix_missing_locations(tmp_tree_victim)
+     #   ast.fix_missing_locations(tmp_tree_victim)
 
         insert_buf = StringIO()
         Unparser(tmp_insert_node, insert_buf)
         insert_buf.seek(0)
         insert_content = insert_buf.read()
         insert_statement_list = insert_content.split('\n')
+        print(insert_statement_list)
         while '' in insert_statement_list:
             insert_statement_list.remove('')
 
-        buf = StringIO()
-        Unparser(tmp_tree_victim, buf)
-        buf.seek(0)
-        edited_content = buf.read()
+        try:
+            buf = StringIO()
+            Unparser(tmp_tree_victim, buf)
+            buf.seek(0)
+            edited_content = buf.read()
+        except IndentationError:
+            can_copy_work=False
 
         with open(combination_path, "w") as combination:
             combination.write(edited_content)
-        result = (run_tests_pv(md5, polluter_fullpath,
+        result = (run_tests_pv(project, md5, polluter_fullpath,
                                combination_path + '::' + '::'.join(victim_fullpath.split('::')[1:])))
 
         tmp_origin_victim.remove(tmp_insert_node)
@@ -139,9 +196,10 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
         patch_num = 0
         patch_time_1st = None
         patch_time_all = None
+        roundnum=0
+        minimal_patch_file= None
         while len(insert_statement_list) >= 2:
             start = 0
-            print('******************************************************')
             subset_length = len(insert_statement_list) // n
             pollution_is_cleaned = False
             while start < len(insert_statement_list):
@@ -150,11 +208,22 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
 
                 tmp_tree_victim = origin_tree_victim
                 tmp_origin_victim = origin_victim_node
-                tmp_insert_node = ast.parse(this_round_insert_code)
 
-                tmp_origin_victim.insert(0, tmp_insert_node)
-                ast.fix_missing_locations(tmp_tree_victim)
 
+                print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+                print(this_round_insert_code)
+
+                try:
+
+                    tmp_insert_node = ast.parse(this_round_insert_code)
+
+
+                    tmp_origin_victim.insert(0, tmp_insert_node)
+
+                   # ast.fix_missing_locations(tmp_tree_victim)
+                    can_be_inserted=True
+                except IndentationError:
+                    can_be_inserted=False
                 tmp_buf = StringIO()
                 Unparser(tmp_insert_node, tmp_buf)
                 tmp_buf.seek(0)
@@ -163,18 +232,24 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
                 # print('@@@@@@@@@@@@@@@@@@@@')
                 # print(tmp_content)
 
+                #if can_be_inserted:
                 buf = StringIO()
                 Unparser(tmp_tree_victim, buf)
                 buf.seek(0)
                 edited_content = buf.read()
 
-                combination_path = combination_path.replace('id', md5)
+                combination_path = combination_path.replace('patch', 'patch'+str(roundnum))
+                roundnum+=1
+                print('~~~~~~~~~~~@@@@@@@@@@@@@@@@@@@@',roundnum)
                 with open(combination_path, "w") as combination:
                     combination.write(edited_content)
 
-                tmp_origin_victim.remove(tmp_insert_node)
-                can_patch_work=(run_tests_pv(md5, polluter_fullpath, combination_path + '::' + '::'.join(
-                        victim_fullpath.split('::')[1:])))
+                if can_be_inserted:
+                    tmp_origin_victim.remove(tmp_insert_node)
+                can_patch_work=(run_tests_pv(project,md5, polluter_fullpath, combination_path + '::' + '::'.join(victim_fullpath.split('::')[1:])))
+               # shutil.copyfile(combination_path, CACHE_DIR+combination_path)
+                cache_in_tests.append(combination_path)
+
                 if can_patch_work.strip() == 'passed':
                     patch_time = time.perf_counter()
                     patch_num += 1
@@ -182,6 +257,7 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
                         patch_time_1st = patch_time - start_time
                     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CAN BE A PATCH~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',
                           tmp_content)
+                    minimal_patch_file=combination_path
                     patch_list.append(tmp_content)
                     insert_statement_list = this_round_insert_list
                     n = max(n - 1, 2)
@@ -193,9 +269,15 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
                 if n == len(insert_statement_list):
                     break
         end_time = time.perf_counter()
-        patch_time_all = end_time - start_time
+        if patch_time_1st:
+            patch_time_all = end_time - start_time
 
-    
+
+#    if minimal_patch_file:
+#        print(victim_path,minimal_patch_file)
+#        generate_diff(victim_path,minimal_patch_file)
+        
+
     with open(result_path,'a+') as f:
         csv_write = csv.writer(f)
         result=[project,sha,polluter_fullpath,cleaner_fullpath,victim_fullpath,md5,pv_result,pcv_result,can_copy_work,patch_time_1st,patch_time_all]
@@ -204,7 +286,20 @@ def fix_victim(project,sha,polluter_fullpath, cleaner_fullpath, victim_fullpath,
             result.append(each)
         
         csv_write.writerow(result)
-       
+    for each in cache_in_tests:
+        if each != minimal_patch_file:
+            os.remove(each)
+
+def generate_diff(original_victim,fixed_victim):
+
+    original_victim_file=open(original_victim,'r')
+    fixed_victim_file=open(fixed_victim,'r')
+    diff=difflib.ndiff(original_victim_file.readlines(),fixed_victim_file.readlines())
+    delta= ''.join(x[2:] for x in diff if x.startswith('- '))
+    print(delta)
+
+
+    
 def get_md5(polluter_fullpath, cleaner_fullpath, victim_fullpath):
     md5=hashlib.md5()
     md5.update(polluter_fullpath.encode())
@@ -215,23 +310,23 @@ def get_md5(polluter_fullpath, cleaner_fullpath, victim_fullpath):
     return md5_Digest
     
         
-def run_tests_pv(md5, polluter_fullpath, victim_fullpath):
-    pv_arg=pytest.main([polluter_fullpath,victim_fullpath,'--csv',CACHE_DIR+'/{}.csv'.format(md5)])
+def run_tests_pv(project,md5, polluter_fullpath, victim_fullpath):
+    pv_arg=[polluter_fullpath,victim_fullpath,'--csv',CACHE_DIR+project+'/{}.csv'.format(md5)]
     capture = io.StdCapture()
     pytest.main(pv_arg)
     capture.reset()
-    csv_result=pd.read_csv(CACHE_DIR + '/{}.csv'.format(md5))
+    csv_result=pd.read_csv(CACHE_DIR +project+ '/{}.csv'.format(md5))
     status=csv_result['status']
     pv_result=status[len(status)-1]
 
     return pv_result
 
-def run_tests_pcv(md5, polluter_fullpath, cleaner_fullpath, victim_fullpath):
-    pcv_arg = pytest.main([polluter_fullpath,cleaner_fullpath,victim_fullpath, '--csv', CACHE_DIR + '/{}.csv'.format(md5)])
-    capture = io.StdCapture()
+def run_tests_pcv(project, md5, polluter_fullpath, cleaner_fullpath, victim_fullpath):
+    pcv_arg = [polluter_fullpath,cleaner_fullpath,victim_fullpath, '--csv', CACHE_DIR +project+ '/{}.csv'.format(md5)]
+    # capture = io.StdCapture()
     pytest.main(pcv_arg)
-    capture.reset()
-    csv_result = pd.read_csv(CACHE_DIR + '/{}.csv'.format(md5))
+    # capture.reset()
+    csv_result = pd.read_csv(CACHE_DIR +project+ '/{}.csv'.format(md5))
     status = csv_result['status']
     pcv_result = status[len(status)-1]
 
