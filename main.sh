d88a41dae3e3a79deadaa565597eba7d3e04ea8d
root <root@4f285643609d>
2021-12-01 02:08:41 +0000

    update

--- main.sh
+++ main.sh
@@ -1,19 +1,17 @@
+input_dataset=$1
+
 pip install pandas
 pip install py
 
 mkdir -p repro/src
 
-cp dataset_trial.csv repro/src/dataset_final.csv
+cp input_dataset repro/src/dataset_final.csv
 
 bash batch.sh $(pwd)/repro/src/dataset_final.csv $(pwd)/Repo $(pwd)/repro/src/isolated test_list 1 0
 
 python3 isolated.py repro/src/dataset_final.csv repro/src/
 
 bash exec_idflakies.sh
-
-#python3 idflakies.py
-
-#python3 -m random_parsing.py
 
 bash batch.sh $(pwd)/repro/src/dataset_final.csv $(pwd)/Repo $(pwd)/repro/src/polluter test_list 2 0
 
@@ -22,8 +20,6 @@ python3 polluter_or_state_setter.py repro/src
 python3 idflakies.py
 
 python3 random_parsing.py
-
-read a
 
 bash find_cleaner.sh repro/src/polluters.csv $(pwd)/Repo test_list $(pwd)/repro/src 3
 
